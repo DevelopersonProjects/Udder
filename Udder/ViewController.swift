2027389b209d6ef8710592782d58d9f386e3ae57
Adam <aajflynn@gmail.com>
2020-10-08 10:29:40 +0100

    Initial commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Adam Flynn on 08/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Initial View Controller loaded.")
+    }
+
+
+}
+
